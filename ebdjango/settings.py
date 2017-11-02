054c8de3dacaceb94c8b78a17d916ab0b2be8e76
Ruban Maher <maher@live.com.sg>
2017-11-03 00:54:48 +0800

    python environment 3.4 set, mysql3.4 connector added

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'CHANGE_ME'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testappapp.nedccmpyec.ap-southeast-1.elasticbeanstalk.com']
 
 
 # Application definition
@@ -75,13 +75,24 @@ WSGI_APPLICATION = 'ebdjango.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'OPTIONS': {'charset': 'utf8mb4'},
+        'NAME': 'testdb',
+		'USER': 'testdba',
+		'PASSWORD': 'testdba_admin',
+		'HOST': 'testdbinstance.ccoelatjjhl3.ap-southeast-1.rds.amazonaws.com',
+        'PORT': '3306',  
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.9/ref/settings/#auth-password-validators
