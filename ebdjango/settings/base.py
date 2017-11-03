4a60c6bf0ef65d7a3310671288766a7286bd8421
Ruban Maher <maher@live.com.sg>
2017-11-03 16:01:03 +0800

    Homepage created, issues with static files fixed

--- ebdjango/settings/base.py
+++ ebdjango/settings/base.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = (
 	('css', os.path.join(STATIC_ROOT , 'css')),
 	('js', os.path.join(STATIC_ROOT , 'js')),
