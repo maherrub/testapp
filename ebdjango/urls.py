71a5cebcfa59bca9da65e9aafc5c7e03c3c4b374
Ruban Maher <maher@live.com.sg>
2017-11-08 11:55:37 +0800

    video player and css update done. no CDN yet.

--- ebdjango/urls.py
+++ ebdjango/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
    url(r'^admin/', admin.site.urls),
    url(r'^', include('helloworld.urls')),
    url(r'^blog/', include('blog.urls', namespace='blog', app_name='blog')), 
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) 
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
